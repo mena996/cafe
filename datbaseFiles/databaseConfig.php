6c2ce44dbb47a0e1a54f0c881ca53c998b095066
mostafaElkenany <mostafa.elkenany@gmail.com>
2020-02-24 20:33:10 +0200

    -Edited databaseConfig file

--- datbaseFiles/databaseConfig.php
+++ datbaseFiles/databaseConfig.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:dbname=Cafe;host=127.0.0.1;port=3306;';
+    $dsn = 'mysql:dbname=cafe;host=127.0.0.1;port=3306;';
     $user = 'root';
-    $password = 'Azayem_242007';
+    $password = '';
     try{
         $db = new PDO($dsn , $user, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
